67afa65d08262a1f172212756b6307b7c1baa9a5
Sohni Uthra <su58@cornell.edu>
2022-03-17 12:38:34 -0400

    fixed health friend and mission statement

--- frontend/src/assets/images/projectDescriptionImages/projectDescriptionImages.js
+++ frontend/src/assets/images/projectDescriptionImages/projectDescriptionImages.js
@@ -2,4 +2,6 @@ export default {
   calmer1: require("./calmer1.jpeg"),
   calmer2: require("./calmer2.jpeg"),
   mycotox: require("./mycotox.png"),
+  healthfriend1: require("./healthfriend1.png"),
+  healthfriend2: require("./healthfriend2.png"),
 };
